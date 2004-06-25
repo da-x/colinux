34dd4755fd20bff587b4ad06e209e77e41f1f02c
Dan Aloni <alonid@gmail.com>
2004-06-25 06:53:41 +0000

    [stable @ 6cc7184618a4aab15ca7e8d4d6485617b32927cc] remove some old days DDK hack
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/stable@79 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/kernel/ddk.h
+++ src/colinux/os/winnt/kernel/ddk.h
@@ -16,22 +16,4 @@
 #include <ddk/winddk.h>
 #include <ddk/ntddk.h>
 
-#if (0)
-#undef IoCompleteRequest 
-
-static inline void
-IoCompleteRequest(
-  IN PIRP  Irp,
-  IN CCHAR  PriorityBoost)
-{
-    asm(
-	"movl %0, %%ecx\n"
-	"movb %1, %%dl\n"
-	"movl __imp_@IofCompleteRequest@8, %%eax\n"
-	"call *%%eax\n"
-	: 
-	: "m" (Irp), "m" (PriorityBoost));
-}
-#endif
-
 #endif
