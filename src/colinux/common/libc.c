be79de95b7a809eeaaee5e67c949eb10754eb33c
Dan Aloni <alonid@gmail.com>
2004-06-25 04:43:58 +0000

    [stable @ 66777e0c2cfc52a38b168cfa96c104658f5f9883] add src/colinux/common/libc.c
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/stable@73 688f6386-cf32-0410-9ead-a596f0484e40

---
+++ src/colinux/common/libc.c
@@ -0,0 +1,30 @@
+/*
+ * This source code is a part of coLinux source package.
+ *
+ * Dan Aloni <da-x@colinux.org>, 2003 (c)
+ *
+ * The code is licensed under the GPL. See the COPYING file at
+ * the root directory.
+ */
+
+#include <stdlib.h>
+#include <strings.h>
+#include <memory.h>
+
+#include "libc.h"
+
+void *co_memset(void *s, int c, long n)
+{
+	return memset(s, c, n);
+}
+
+void *co_memcpy(void *dest, const void *src, long n)
+{
+	return memcpy(dest, src, n);
+}
+
+void co_bzero(void *s, long n)
+{
+	bzero(s, n);
+}
+
