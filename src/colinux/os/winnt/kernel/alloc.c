c5f528f91abbda59e41abd4b1da45b19ef35e67a
Dan Aloni <alonid@gmail.com>
2004-07-17 15:11:57 +0000

    [stable @ 24e2801a6acc4adc5929002fa8699daeb8f2f9fb] fix cobd's alias major registration logic bug ; allow for aliases to have no minor (e.g, cobd2 -> hdd)
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/stable@110 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/kernel/alloc.c
+++ src/colinux/os/winnt/kernel/alloc.c
@@ -1,7 +1,7 @@
 /*
  * This source code is a part of coLinux source package.
  *
- * Dan Aloni <da-x@colinux.org>, 2003 (c)
+ * Dan Aloni <da-x@colinux.org>, 2003-2004 (c)
  *
  * The code is licensed under the GPL. See the COPYING file at
  * the root directory.
