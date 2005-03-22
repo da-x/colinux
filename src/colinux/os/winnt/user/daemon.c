d6cef950d1531e5354762485b302627b9a4fc75d
George Boutwell <george.boutwell@gmail.com>
2005-03-22 14:37:48 +0000

    [org.colinux.stable @ d12e06c48d5b71144e573265c29d58f6f4ec22f2]   * Backport of kernel panic fix form 0.7.1 (devel)   * Revert bjh i/o improvement patch, causes heavy CPU usage on Win2k.
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/stable@264 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/user/daemon.c
+++ src/colinux/os/winnt/user/daemon.c
@@ -15,31 +15,12 @@
 #include <colinux/os/alloc.h>
 #include <colinux/common/messages.h>
 
-static unsigned totalloc;
-
-/*
- * Maximum/Minimum memory size of packet allocation heap.
- */
-
-#define PACKET_HEAP_SIZE_MIN 0x10000
-#define PACKET_HEAP_SIZE_MAX 0x100000
-
 /*
  * Queue iteration threshold for packet retention and deallocation
  */
  
 #define PACKET_RETENTION 3
 
-/*
- * Intraprocess synchronization
- */
- 
-static CRITICAL_SECTION io_sync;
- 
-/*
- * Data packet structure with single-linked list
- */
- 
 typedef struct _packet {
 	struct _packet *next;
 	unsigned size;
@@ -47,51 +28,18 @@ typedef struct _packet {
 	co_message_t message[0];
 } packet_t;
 
-/*
- * Finite state enumeration of I/O operations in io_thread()
- */
-
-typedef enum {
-	io_read = 1,
-	io_read_submit,
-	io_wait,
-	io_wait_resources,
-	io_read_more,
-	io_read_burst,
-	io_read_packet,
-	io_queue,
-	io_queue_burst,
-	io_queue_burst_loop,
-	io_error
-} io_state;
-
-/*
- * Data passed between asynchronous procedure calls and io_thread()
- */
-
-typedef struct {
-	co_daemon_handle_t daemon;
-	co_rc_t rc;
-	io_state iostate;
-	io_state return_state;
-	unsigned transferred;
-	unsigned transfer;
-} io_data;
-
 static packet_t *
 packet_allocate(co_daemon_handle_t daemon, size_t size)
 {
 	packet_t *packet = daemon->packets;
 	packet_t **shadow = (packet_t**)&daemon->packets;
 
-	EnterCriticalSection(&io_sync);
 	while(packet) {
 		if(packet->free) {
 			if(packet->size >= size && size >= (packet->size >> 1)) {
 				packet->free = 0;
 				*shadow = packet->next;
 				packet->next = 0;
-				LeaveCriticalSection(&io_sync);
 				return packet;
 			}
 			if(++packet->free <= PACKET_RETENTION) {
@@ -101,73 +49,20 @@ packet_allocate(co_daemon_handle_t daemon, size_t size)
 			}
 			*shadow = packet->next;
 			HeapFree(daemon->heap, HEAP_NO_SERIALIZE, packet);
-			totalloc--;
 			packet = *shadow;
 			continue;
 		}
 		shadow = &packet->next;
 		packet = packet->next;
 	}
-	LeaveCriticalSection(&io_sync);
 
 	if(!(packet = HeapAlloc(daemon->heap, HEAP_NO_SERIALIZE, sizeof(packet_t)+size)))
 		return NULL;
-	totalloc++;
 	packet->next = 0;
 	packet->free = 0;
 	packet->size = size;
 	return packet;
 }
-
-static bool_t
-packet_reallocate(co_daemon_handle_t daemon, packet_t **packet, size_t size)
-{
-	// UPDATE: reallocate within range and 
-	if((*packet)->size >= size)
-		return PTRUE;
-	if(HeapReAlloc(daemon->heap, HEAP_REALLOC_IN_PLACE_ONLY, *packet, sizeof(packet_t)+size)) {
-		(*packet)->size = size;
-		return PTRUE;
-	}
-	packet_t *newpacket;
-	if(!(newpacket = packet_allocate(daemon, size)))
-		return PFALSE;
-	memcpy(newpacket->message, (*packet)->message, (*packet)->size);
-	EnterCriticalSection(&io_sync);
-	(*packet)->free = 1;
-	(*packet)->next = daemon->packets;
-	daemon->packets = *packet;
-	LeaveCriticalSection(&io_sync);
-	*packet = newpacket;
-	return PTRUE;
-}
-
-static void CALLBACK io_readCompletion(DWORD err, DWORD transferred, LPOVERLAPPED overlapped)
-{
-	io_data *iodata = (io_data*)overlapped->hEvent;
-	if (!transferred) {
-		if (err == ERROR_BROKEN_PIPE) {
-			iodata->iostate = io_error;
-			iodata->rc = CO_RC(BROKEN_PIPE);
-			return;
-		}
-		if (err != ERROR_SUCCESS) {
-			co_debug_lvl(pipe,10,"io error %x",err);
-			iodata->iostate = io_error;
-			iodata->rc = CO_RC(ERROR);
-			return;
-		}
-	}
-	if ((iodata->transferred += transferred) != iodata->transfer) {
-		co_debug_lvl(pipe,10,"io transfer larger than requested itd=%d td=%d t=%d", iodata->transferred, transferred, iodata->transfer);
-		iodata->iostate = io_error;
-		iodata->rc = CO_RC(ERROR);
-		return;
-	}
-	iodata->iostate = iodata->return_state;
-	iodata->rc = CO_RC(OK);
-}
-
 
 static DWORD WINAPI io_thread(LPVOID param)
 {
@@ -176,207 +71,160 @@ static DWORD WINAPI io_thread(LPVOID param)
 	OVERLAPPED overlapped;
 	bool_t async = PFALSE;
 	packet_t *packet = 0;
+	HANDLE events[2];
 	packet_t *p = 0;
-	co_message_t *message = 0;
-	io_state iostate;
-	io_state return_state = 0;
-	io_data iodata;
 	co_rc_t rc;
 	DWORD size;
 	DWORD code;
-	unsigned burst_size = 0;
 
-	co_debug_lvl(pipe,10,"io thread started");
-	daemon->heap = HeapCreate(HEAP_NO_SERIALIZE, PACKET_HEAP_SIZE_MIN, PACKET_HEAP_SIZE_MAX);
-//	HeapSetInformation(daemon->heap, HeapCompatibilityInformation, &(unsigned long)2, sizeof(unsigned long);
+#	define NT_ERROR(_1) { \
+		co_debug_lvl(pipe,1,"co_os_daemon_thread() error [ %x , %s ]\n", \
+			GetLastError(), \
+			_1); \
+		goto co_os_daemon_thread_error; \
+	}
+	
+	daemon->qIn = daemon->qOut = daemon->packets = 0;
+	co_debug_lvl(pipe,10,"user daemon thread started");
+	daemon->heap = HeapCreate(HEAP_NO_SERIALIZE, 0x10000, 0x100000);
+
 	memset(&overlapped, 0, sizeof (overlapped));
-	overlapped.hEvent = &iodata;
-	iodata.iostate = io_wait;
-	iodata.daemon = daemon;
-	rc = CO_RC(OK);
+	overlapped.hEvent = CreateEvent(0, TRUE, FALSE, 0);
 
-	iostate = io_wait;
+	if (!overlapped.hEvent)
+		NT_ERROR("CE001");
+
+	events[0] = overlapped.hEvent;
+	events[1] = daemon->shifted;
+
+      co_os_daemon_thread_loop:
+	if (WaitForSingleObject(events[1], INFINITE) == WAIT_FAILED)
+		NT_ERROR("W0001");
+
 	while (daemon->loop) {
-		switch(iostate) {
-			case io_read: {
-				if (!(packet = packet_allocate(daemon,sizeof(co_message_t)))) {
-					return_state = io_read;
-					iostate = io_wait_resources;
-					continue;
-				}
-				iodata.transferred = 0;
-				iodata.transfer = sizeof(co_message_t);
-				iodata.return_state = io_read_more;
-			}
-			case io_read_submit: {
-				if (!ReadFileEx(pipe, (void*)packet->message + iodata.transferred, iodata.transfer - iodata.transferred, &overlapped, io_readCompletion)) {
-					co_debug_lvl(pipe,10,"io read error %d", GetLastError());
-					iostate = io_error;
-					rc = CO_RC(ERROR);
-					break;
-				}
-				async = PTRUE;
-				iostate = io_wait;
-			}
-			case io_wait: {
-				code = WaitForSingleObjectEx(daemon->shifted, INFINITE, TRUE);
-				if(code == WAIT_FAILED) {
-					iostate = io_error;
-					rc = CO_RC(ERROR);
-					break;
-				}
-				if(code == WAIT_IO_COMPLETION && iodata.iostate) {
-					async = PFALSE;
-					if ((iostate = iodata.iostate) == io_error) {
-						rc = iodata.rc;
-						break;
-					}
-					iodata.iostate = 0;
-					continue;
-				}
-				if(!async)
-					iostate = io_read;
+		if ((!daemon->message) && daemon->qOut) {
+			p = daemon->qOut;
+			if(! (daemon->qOut = p->next) )
+				daemon->qIn = 0;
+			p->next = daemon->packets;
+			daemon->packets = p;
+			daemon->message = p->message;
+			// p = 0;
+			if (!SetEvent(daemon->readable))
+				NT_ERROR("SE0001");
+		}
+		if (async) {
+			code = WaitForMultipleObjects(2, events, FALSE, INFINITE);
+			if(code == WAIT_FAILED)
+				NT_ERROR("W0002");
+			if (!daemon->message)
+				if (!ResetEvent(daemon->readable))
+					NT_ERROR("RE0001");
+			if (code != WAIT_OBJECT_0)
 				continue;
-			}
-			case io_wait_resources: {
-				HeapCompact(daemon->heap, HEAP_NO_SERIALIZE);
-				code = WaitForSingleObject(daemon->shifted, 10);  // UPDATE: avoid busy wait: timeout
-				if(code == WAIT_FAILED) {
-					iostate = io_error;
-					rc = CO_RC(ERROR);
-					break;
-				}
-				iostate = return_state;
-				continue;
-			}
-			case io_read_more: {
-				if (!PeekNamedPipe(pipe, 0, 0, 0, &size, 0)) {
-					iostate = io_error;
-					rc = CO_RC(ERROR);
-					break;
-				}
-				if ((iodata.transfer += size) <= packet->message->size) {
-					iostate = io_read_packet;
+			if (!GetOverlappedResult(pipe, &overlapped, &size, FALSE)) {
+				code = GetLastError();
+				if (code == ERROR_IO_INCOMPLETE)
 					continue;
-				}
-				burst_size = PACKET_HEAP_SIZE_MAX >> 1;
-				if (iodata.transfer > burst_size)
-					iodata.transfer = burst_size;
-			}
-			case io_read_burst: {
-				if (!packet_reallocate(daemon, &packet, iodata.transfer)) {
-					if ((burst_size >>= 1) < PACKET_HEAP_SIZE_MIN) {
-						iodata.transfer = sizeof(co_message_t) + packet->message->size;
-						return_state = io_read_packet;
-						iostate = io_wait_resources;
-						continue;
-					}
-					if (iodata.transfer > burst_size)
-						iodata.transfer = burst_size;
-					return_state = io_read_burst;
-					iostate = io_wait_resources;
+				if (code == ERROR_IO_PENDING)
 					continue;
-				}
-				iodata.return_state = io_queue_burst;
-				iostate = io_read_submit;
-				continue;
+				if (code == ERROR_BROKEN_PIPE)
+					goto co_os_daemon_thread_broken_pipe;
+				NT_ERROR("GOR01");
 			}
-			case io_read_packet: {
-				if (!packet_reallocate(daemon, &packet, iodata.transfer)) {
-					return_state = io_read_packet;
-					iostate = io_wait_resources;
-					continue;
+			if(!size)	{
+				packet->free = 1;
+				packet->next = daemon->packets;
+				daemon->packets = packet;
 				}
-				iodata.return_state = io_queue;
-				iostate = io_read_submit;
-				continue;
-			}
-			case io_queue: {
-				EnterCriticalSection(&io_sync);
-				if(!daemon->qOut)
+			else	{
+				if(size < sizeof(co_message_t))
+					NT_ERROR("DS0001");
+				if(size != packet->message->size + sizeof(co_message_t))
+					NT_ERROR("DS0002");
+				if(!daemon->qOut) {
 					daemon->qOut = daemon->qIn = packet;
-				else {
+				} else {
 					((packet_t*)daemon->qIn)->next = packet;
 					daemon->qIn = packet;
 				}
-				if (!SetEvent(daemon->readable)) {
-					LeaveCriticalSection(&io_sync);
-					iostate = io_error;
-					rc = CO_RC(ERROR);
-					break;
-				}
-				LeaveCriticalSection(&io_sync);
-				iostate = io_read;
-				continue;
 			}
-			case io_queue_burst: {
-				size = 0;
-				message = packet->message;
-				iostate = io_queue_burst_loop;
-			}
-			case io_queue_burst_loop: {
-				if (!(p = packet_allocate(daemon,sizeof(co_message_t)+message->size))) {
-					return_state = io_queue_burst_loop;
-					iostate = io_wait_resources;
-					continue;
-				}
-				memcpy(p->message, message, sizeof(co_message_t)+message->size);
-				EnterCriticalSection(&io_sync);
-				if (!daemon->qOut)
-					daemon->qOut = daemon->qIn = p;
-				else {
-					((packet_t*)daemon->qIn)->next = p;
-					daemon->qIn = p;
-				}
-				if (!SetEvent(daemon->readable)) {
-					LeaveCriticalSection(&io_sync);
-					iostate = io_error;
-					rc = CO_RC(ERROR);
-					break;
-				}
-				if ((size += message->size + sizeof(co_message_t)) == iodata.transferred) {
-					packet->free = 1;
-					packet->next = daemon->packets;
-					daemon->packets = packet;
-					LeaveCriticalSection(&io_sync);
-					// packet = 0;
-					iostate = io_read;
-					continue;
-				}
-				LeaveCriticalSection(&io_sync);
-				message = (void*)packet->message + size;
-				if (size + sizeof(co_message_t) <= iodata.transferred)
-					if (size + sizeof(co_message_t) + message->size <= iodata.transferred) {
-						iostate = io_queue_burst_loop;
-						continue;
-					}
-				iodata.transferred -= size;
-				memcpy(packet->message, message, iodata.transferred);
-				if (iodata.transferred >= sizeof(co_message_t)) {
-					iodata.transfer = iodata.transferred;
-					iostate = io_read_more;
-					continue;
-				}
-				iodata.transfer = sizeof(co_message_t);
-				iodata.return_state = io_read_more;
-				iostate = io_read_submit;
-				continue;
-			}
-			case io_error: break;
+			// packet = 0;
+			async = PFALSE;
 		}
-		if(iostate == io_error)
-			break;
+		if (!async) {
+			if (!PeekNamedPipe(pipe, 0, 0, 0, 0, &size))
+				NT_ERROR("PNP01");
+			if (!(packet = packet_allocate(daemon,size)))
+				goto co_os_daemon_thread_loop;
+			async = PTRUE;
+			if (!ReadFile(pipe, packet->message, size, 0, &overlapped)) {
+				code = GetLastError();
+				if (code == ERROR_IO_INCOMPLETE)
+					continue;
+				if (code == ERROR_IO_PENDING)
+					continue;
+				if (code == ERROR_BROKEN_PIPE)
+					goto co_os_daemon_thread_broken_pipe;
+				NT_ERROR("RF001");
+			}
+		}
 	}
 
+	rc = CO_RC(OK);
+
+	co_os_daemon_thread_return:
 	if (async) {
 		CancelIo(pipe);
+		HeapFree(daemon->heap, HEAP_NO_SERIALIZE, packet);
 		async = 0;
 	}
+	while (daemon->loop && daemon->qOut) {
+		if (!daemon->message) {
+			p = daemon->qOut;
+			if(! (daemon->qOut = p->next) )
+				daemon->qIn = 0;
+			p->next = daemon->packets;
+			daemon->packets = p;
+			daemon->message = p->message;
+			// p = 0;
+			if (!SetEvent(daemon->readable)) {
+				if (!CO_OK(rc))
+					break;
+				NT_ERROR("SE0002");
+			}
+		}
+		if (WaitForSingleObject(events[1], INFINITE) == WAIT_FAILED) {
+			if (!CO_OK(rc))
+				break;
+			NT_ERROR("W0003");
+		}
+		if (!daemon->message)
+			if (!ResetEvent(daemon->readable)) {
+				if (!CO_OK(rc))
+					break;
+				NT_ERROR("RE0002");
+			}
+	}
+	if (overlapped.hEvent)
+		if ((!CloseHandle(overlapped.hEvent)) && CO_OK(rc))
+			co_debug_lvl(pipe,1,"co_os_daemon_thread() error"
+				 " [ %x , CH0001 ]\n", GetLastError());
 	daemon->rc = rc;
 	if (daemon->loop)
 		if ((!SetEvent(daemon->readable)) && CO_OK(rc))
-			co_debug_lvl(pipe,1,"error");
+			co_debug_lvl(pipe,1,"co_os_daemon_thread() error"
+				 " [ %x , SE0003 ]\n", GetLastError());
+
 	return 0;
+
+      co_os_daemon_thread_error:
+	rc = CO_RC(ERROR);
+	goto co_os_daemon_thread_return;
+
+      co_os_daemon_thread_broken_pipe:
+	rc = CO_RC(BROKEN_PIPE);
+	goto co_os_daemon_thread_return;
 }
 
 co_rc_t
@@ -392,40 +240,35 @@ co_rc_t
 co_os_daemon_message_receive(co_daemon_handle_t daemon,
 			 co_message_t ** message_out, unsigned long timeout)
 {
+	DWORD r;
 	co_rc_t rc = daemon->rc;
-	packet_t *packet;
-	DWORD code;
 
 	*message_out = NULL;
 
-	if((!daemon->qOut) && CO_OK(rc)) {
-		code = MsgWaitForMultipleObjects(1, &daemon->readable, FALSE, timeout, QS_ALLEVENTS);
-		if (code == WAIT_OBJECT_0 + 1)
-			return CO_RC(TIMEOUT);
-		if (code == WAIT_TIMEOUT) {
-			SetEvent(daemon->shifted);
-			return CO_RC(TIMEOUT);
-		}
-	}
-	if(daemon->qOut) {
-		EnterCriticalSection(&io_sync);
-		packet = daemon->qOut;
-		daemon->qOut = packet->next;
-		packet->next = daemon->packets;
-		daemon->packets = packet;
-		*message_out = packet->message;
-		if(!daemon->qOut) {
-			if (!ResetEvent(daemon->readable)) {
-				LeaveCriticalSection(&io_sync);
-				return CO_RC(ERROR);
-			}
-//			SetEvent(daemon->shifted);
-		}
-		LeaveCriticalSection(&io_sync);
+	if (daemon->message) {
+		*message_out = daemon->message;
+		daemon->message = 0;
 		return CO_RC(OK);
 	}
+
+	if (!CO_OK(rc))
+		return rc;
+
 	SetEvent(daemon->shifted);
-	return rc;
+
+	r = MsgWaitForMultipleObjects(1, &daemon->readable, FALSE, timeout,
+				      QS_ALLEVENTS);
+	if (r == WAIT_TIMEOUT || r == WAIT_OBJECT_0 + 1) {
+		return CO_RC(TIMEOUT);
+	}
+
+	if (daemon->message) {
+		*message_out = daemon->message;
+		daemon->message = 0;
+	}
+	SetEvent(daemon->shifted);
+
+	return CO_RC(OK);
 }
 
 co_rc_t
@@ -484,17 +327,15 @@ co_os_daemon_pipe_open(co_id_t linux_id, co_module_t module_id,
 		goto co_os_open_daemon_pipe_error;
 	}
 
-	InitializeCriticalSection(&io_sync);
-
 	if (!(daemon = co_os_malloc(sizeof (*daemon))))
 		goto co_os_open_daemon_pipe_error;
 
 	daemon->handle = handle;
 	daemon->loop = 1;
 	daemon->rc = CO_RC(OK);
+	daemon->message = 0;
 	daemon->shifted = CreateEvent(0, FALSE, FALSE, 0);
 	daemon->readable = CreateEvent(0, TRUE, TRUE, 0);
-	daemon->qIn = daemon->qOut = daemon->packets = 0;
 	if (!(daemon->shifted && daemon->readable))
 		goto co_os_open_daemon_pipe_error;
 
@@ -539,6 +380,5 @@ co_os_daemon_pipe_close(co_daemon_handle_t daemon)
 	CloseHandle(daemon->thread);
 	HeapDestroy(daemon->heap);
 	co_os_free(daemon);
-	DeleteCriticalSection(&io_sync);
 	return CO_RC(OK);
 }
