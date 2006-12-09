a018a0fb186b37fa065b1111bc538af34aabcc3c
Henry Nestler <henry@bigfoot.de>
2006-12-09 12:11:07 +0000

    [org.colinux.stable @ d76fb2ce88323d55f6fa1d43a38b2b39f959eaed] * Return "invalid parameter" instead general "error" for invalid mac.
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/stable@357 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/user/macaddress.c
+++ src/colinux/user/macaddress.c
@@ -18,9 +18,8 @@ co_rc_t co_parse_mac_address(const char *text, unsigned char *binary)
 	int ret, i;
 	unsigned int mac[6];
 
-	if (strlen(text) != 17) { 
-		return CO_RC(ERROR);
-	}
+	if (strlen(text) != 17) 
+		return CO_RC(INVALID_PARAMETER);
 	
 	ret = sscanf(text, "%02x:%02x:%02x:%02x:%02x:%02x", 
 		     &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
@@ -29,7 +28,7 @@ co_rc_t co_parse_mac_address(const char *text, unsigned char *binary)
 		binary[i] = mac[i];
 
 	if (ret != 6)
-		return CO_RC(ERROR);
+		return CO_RC(INVALID_PARAMETER);
 
 	return CO_RC(OK);
 }
