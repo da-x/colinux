d188467e9f09930d25c4fd728653411032f981af
Henry Nestler <henry@bigfoot.de>
2007-03-24 14:48:48 +0000

    [org.colinux.devel @ 049dc6131e55eeb8309c5d0e616bb7ad87b51f6b] * Execute colinux_def.sh directly, not as 'source'.   Parameters $1 and $2 can give for the shell dash now. * File colinux_def.sh set executable.
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/devel@595 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/user/install/colinux_def.sh
+++ src/colinux/os/winnt/user/install/colinux_def.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
+# This File exports the current version informations and
+# creates an NIS installer include file.
+
 # Remember: This file will be sourced from / as current directory.
 # Standard arguments for this script:
-# pwd: $HOME/CoLinux/work/co-devel
-# $0: /bin/sh
+# pwd: $HOME/colinux-devel
 # $1: src/colinux/VERSION
 # $2: src/colinux/os/winnt/user/install/colinux_def.inc
 
 VERSION_FILE=$1
 TARGET_FILE=$2
 
-# Fallback for dash with defaults
-test -z "$VERSION_FILE" && VERSION_FILE="src/colinux/VERSION"
-test -z "$TARGET_FILE" && TARGET_FILE="src/colinux/os/winnt/user/install/colinux_def.inc"
-
 # Get kernel version
-KERNEL_VERSION=`. bin/build-common.sh --get-vars; echo \$KERNEL_VERSION`
+. bin/build-common.sh --get-vars
 
 # coLinux full version with "-preXX", if exist
 PRE_VERSION=`cat $VERSION_FILE`
