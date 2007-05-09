ce6e01ccc3422850bf3fe5dd5710fa90afed898d
Henry Nestler <henry@bigfoot.de>
2007-05-09 20:41:42 +0000

    [org.colinux.devel @ 3821825b3ab7ebb075bc73996d4f1e4ba57794dc] * Resource target depends not on current mtime of directory (build system)
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/devel@609 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/user/daemon/res/build.comake.py
+++ src/colinux/os/winnt/user/daemon/res/build.comake.py
@@ -4,15 +4,14 @@ def script_cmdline(scripter, tool_run_inf):
         "%s -O coff -I %s %s -o %s"
         ) %
     (scripter.get_cross_build_tool('windres', tool_run_inf),
+     current_dirname,
      inputs[0].pathname,
-     inputs[1].pathname,
      tool_run_inf.target.pathname,))
     return command_line
 
 targets['daemon.res'] = Target(
     tool = Script(script_cmdline),
     inputs = [
-       Input('.'),
        Input('daemon.rc'),
        Input('resources_def.inc'),
     ],
@@ -21,7 +20,6 @@ targets['daemon.res'] = Target(
 targets['colinux.res'] = Target(
     tool = Script(script_cmdline),
     inputs = [
-       Input('.'),
        Input('colinux.rc'),
        Input('resources_def.inc'),
     ],
