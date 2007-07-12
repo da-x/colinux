5034fa5e77609c2a3c65c98cce5d25df06eb0b1c
Henry Nestler <henry@bigfoot.de>
2007-07-12 17:19:02 +0000

    * Resource target (daemon.res) depends not on current mtime of directory
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/stable@661 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/user/daemon/res/build.comake.py
+++ src/colinux/os/winnt/user/daemon/res/build.comake.py
@@ -4,15 +4,14 @@ def script_cmdline(scripter, tool_run_inf):
         "%s -O coff -I %s %s -o %s"
         ) %
     (scripter.get_cross_build_tool('windres', tool_run_inf),
+     current_dirname,
      inputs[0].pathname,
-     inputs[1].pathname,
      tool_run_inf.target.pathname,))
     return command_line
 
 targets['daemon.res'] = Target(
     tool = Script(script_cmdline),
     inputs = [
-       Input('.'),
        Input('daemon.rc'),
     ],
 )
