781f99039ce9145a625a574feeb7645a1e21e8a8
Henry Nestler <henry@bigfoot.de>
2008-01-20 14:24:57 +0000

    * Build system: Replace manualy object list with automatic input_list.
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/devel@860 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/user/build.comake.py
+++ src/colinux/os/winnt/user/build.comake.py
@@ -6,19 +6,5 @@ targets['user-all.a'] = Target(
 )
 
 targets['user-osdep.a'] = Target(
-    inputs=[
-    Input("manager.o"),
-    Input("file.o"),
-    Input("file-write.o"),
-    Input("file-unlink.o"),
-    Input("alloc.o"),
-    Input("misc.o"),
-    Input("osdep.o"),
-    Input("timer.o"),
-    Input("exec.o"),
-    Input("udp.o"),
-    Input("cobdpath.o"),
-    Input("reactor.o"),
-    Input("process.o"),
-    ]
+    inputs=input_list(".c", ".o"),
 )
