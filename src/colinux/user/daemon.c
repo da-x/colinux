83efc832096ab0e1c174f1c320b4ef8d06c5bdbb
Henry Nestler <henry@bigfoot.de>
2008-03-02 17:40:39 +0000

    * co_daemon_prepare_net_macs, co_load_initrd, co_daemon_monitor_create: static (backport from r836)
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/stable@930 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/user/daemon.c
+++ src/colinux/user/daemon.c
@@ -261,6 +261,7 @@ co_rc_t co_daemon_load_symbol(co_daemon_t *daemon,
 	return rc;
 }
 
+static
 void co_daemon_prepare_net_macs(co_daemon_t *daemon)
 {
 	int i;
@@ -289,6 +290,7 @@ void co_daemon_prepare_net_macs(co_daemon_t *daemon)
 	}
 }
 
+static
 co_rc_t co_load_initrd(co_daemon_t *daemon)
 {
 	co_rc_t rc;
@@ -347,6 +349,7 @@ memory_usage_limit_resached(co_manager_ioctl_create_t *create_params)
 			  info.hostmem_used / 0x100000);
 }
 
+static
 co_rc_t co_daemon_monitor_create(co_daemon_t *daemon)
 {
 	co_manager_ioctl_create_t create_params = {0, };
