b70fa7801219178779d6bbc9a13c9d9ee5638948
Henry Nestler <henry@bigfoot.de>
2008-03-02 17:56:38 +0000

    * Daemon build system: Replace manualy object list with automatic input_list. (backport from r852)
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/stable@933 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/user/daemon/build.comake.py
+++ src/colinux/os/winnt/user/daemon/build.comake.py
@@ -1,11 +1,3 @@
 targets['daemon.o'] = Target(
-    inputs=[
-    Input('main.o'),
-    Input('debug.o'),
-    Input('service.o'),
-    Input('driver.o'),
-    Input('cmdline.o'),
-    Input('misc.o'),
-    Input('registry.o'),
-    ],
+    inputs = input_list(".c", ".o"),
 )
