97e6fa23456d99728ed707aaf8364fc6b69aeab7
Henry Nestler <henry@bigfoot.de>
2008-12-12 21:46:30 +0000

    * DbgPrint: Add NewLine, to not have all in one.
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/devel@1156 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/kernel/lowlevel/debug.c
+++ src/colinux/os/winnt/kernel/lowlevel/debug.c
@@ -41,7 +41,7 @@ void co_debug_system(const char *fmt, ...)
 
 	va_start(ap, fmt);
 	co_vsnprintf(buf, sizeof(buf), fmt, ap);
-	DbgPrint("%s", buf);
+	DbgPrint("%s\n", buf);
 	va_end(ap);
 }
 
@@ -49,6 +49,6 @@ void co_debug_level_system(const char *module, co_debug_facility_t facility, int
 		    const char *filename, int line, const char *func, const char *text)
 {
 	/* Debug output to Dbgview.exe (www.sysinternals.com) */
-        DbgPrint("[m:%s f:%d l:%d %s:%d:%s] %s", module, facility, level, filename, line, func, text);
+        DbgPrint("[m:%s f:%d l:%d %s:%d:%s] %s\n", module, facility, level, filename, line, func, text);
 }
 #endif
