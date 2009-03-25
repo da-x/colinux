bcf220c822632060737a26d91af1cb250d82db50
Henry Nestler <henry@bigfoot.de>
2009-03-25 21:52:25 +0000

    * With 'COLINUX_NO_CPU0_WORKAROUND=Y' coLinux never use first processor.   Solves problems with heavy interrupts.
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/devel@1238 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/winnt/user/daemon/misc.c
+++ src/colinux/os/winnt/user/daemon/misc.c
@@ -17,13 +17,32 @@
  */
 void co_winnt_affinity_workaround(void)
 {
-	char *env = getenv("COLINUX_NO_SMP_WORKAROUND");
+	char *env;
+	HANDLE hProcess = GetCurrentProcess();
 
-	if (env != NULL) {
-		if (strcmp(env, "Y") == 0)
-			return;
+	// Prevent coLinux from using CPU0
+	env = getenv("COLINUX_NO_CPU0_WORKAROUND");
+	if (env && strcmp(env, "Y") == 0) {
+		DWORD ProcessMask, SystemMask, NewMask;
+
+		if (GetProcessAffinityMask (hProcess, &ProcessMask, &SystemMask)) {
+			NewMask = SystemMask & ~1;
+			co_debug("AffinityMasks Process %lx, System %lx, New %lx", ProcessMask, SystemMask, NewMask);
+			if (NewMask != ProcessMask) {
+				if (!SetProcessAffinityMask(hProcess, NewMask)) {
+					co_debug("SetProcessAffinityMask failed with 0x%lx", GetLastError());
+				}
+			}
+		} else
+			co_debug("GetProcessAffinityMask failed with 0x%lx", GetLastError());
+
+		return;
 	}
 
-	SetProcessAffinityMask(GetCurrentProcess(), 1);
+	env = getenv("COLINUX_NO_SMP_WORKAROUND");
+	if (env && strcmp(env, "Y") == 0)
+		return;
+
+	SetProcessAffinityMask(hProcess, 1);
 }
 
