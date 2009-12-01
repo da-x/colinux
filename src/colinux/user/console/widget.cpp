d9436e43e2616760e8d0d03dadd876e872a044d4
Henry Nestler <henry@bigfoot.de>
2009-12-01 20:36:46 +0000

    * FLTK console: Fix buffer overflow for option cocon=x*y with x > 256 columns.
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/devel@1313 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/user/console/widget.cpp
+++ src/colinux/user/console/widget.cpp
@@ -184,7 +184,7 @@ void console_widget_t::draw()
 		}
 		
 		while (cell < end) {
-			while (cell < end  &&  start->attr == cell->attr) {
+			while (cell < end  &&  start->attr == cell->attr  &&  cell - start < sizeof(text_buff)) {
 				text_buff[cell - start] = cell->ch;
 				cell++;
 			}
