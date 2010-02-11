c7eb38adb0faf93b6e8a730f448a0e925c85205b
sunyin <sunyin@688f6386-cf32-0410-9ead-a596f0484e40>
2010-02-11 05:11:14 +0000

    cofb add os_console.h remove cofb downgrade file
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/cofb@1363 688f6386-cf32-0410-9ead-a596f0484e40

---
+++ src/colinux/os/winnt/user/console-nt/os_console.h
@@ -0,0 +1,22 @@
+
+/* WinNT host: low level console routines */
+
+#ifndef __CO_OS_USER_WINNT_OS_CONSOLE_H__
+#define __CO_OS_USER_WINNT_OS_CONSOLE_H__
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+void co_console_set_cursor_size(void* out_h, const int cursor_type);
+  /* Set cursor size 
+     Parameters:
+       out_h         - console output handle
+       cursor_type   - size of cursor in Linux kernel defines
+  */
+
+#if defined __cplusplus
+}
+#endif
+
+#endif /* __CO_OS_USER_WINNT_OS_CONSOLE_H__ */
