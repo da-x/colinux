23ba8fb6fd3792054e8c88a1f070e0ad3398cf9b
Henry Nestler <henry@bigfoot.de>
2010-11-30 22:48:46 +0000

    * Linux as host: Strip out guest kernel include paths for tap.c builds.
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/devel@1562 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/os/linux/user/conet-daemon/build.comake.py
+++ src/colinux/os/linux/user/conet-daemon/build.comake.py
@@ -1,3 +1,14 @@
+# Strip out guest kernel include paths for tap.c builds
+targets['tap.o'] = Target(
+    inputs = [Input('tap.c')],
+    tool = Compiler(),
+    mono_options = Options(
+        overriders = dict(
+            compiler_includes = []
+        )
+    )
+)
+
 targets['build.o'] = Target(
     inputs=[
        Input('daemon.o'),
