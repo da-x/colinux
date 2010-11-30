201ca83d568269f6b5fdfd6742ee2d58b488865c
Henry Nestler <henry@bigfoot.de>
2010-11-30 20:44:51 +0000

    * FLTK: strnset is not portable and not need before strcpy. (Linux as host)
    git-svn-id: svn://svn.code.sf.net/p/colinux/code/branches/devel@1558 688f6386-cf32-0410-9ead-a596f0484e40

--- src/colinux/user/console-fltk/fontselect.cpp
+++ src/colinux/user/console-fltk/fontselect.cpp
@@ -62,7 +62,6 @@ FontSelectDialog::FontSelectDialog(void*ptr, int def_font, int def_size): Fl_Win
 	numsizes = NULL;
 	lookup = NULL;
 	num_fonts = 0;
-	strnset(str_label, 0, 0x200);
 
 	strcpy(str_label, DISPLAY_STR);
 	//init_display_string();
